e2389ff82824e540c449108b5743656cbdea8025
Mohammed Hussain <80367091+MSH-01@users.noreply.github.com>
2022-09-16 16:32:01 +0100

    Added create image folder function and fixed ui

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+let chosenFolder = "";
+const fs = require('uxp').storage.localFileSystem;
+
 async function pickWorkingDirectory(){
   //const app = require('photoshop').app;
-  const fs = require('uxp').storage.localFileSystem;
   let folder = await fs.getFolder();
   let token = fs.createSessionToken(folder);
   console.log(token);
   console.log(folder);
+  document.getElementById("directory-lbl").innerHTML = folder.nativePath;
+  chosenFolder = folder;
+}
 
-  document.getElementById("directory-lbl").innerHTML = folder.nativePath
-
+async function createImageFolder(){
+  let numberOfFolders = document.getElementById("image-folder-no").value;
+  if(numberOfFolders == "" || 0){
+    document.getElementById("warning-lbl").innerHTML = "Enter number of folders";
+  }else if(chosenPath == ""){
+    document.getElementById("warning-lbl").innerHTML = "Pick a working directory";
+  }else{
+    document.getElementById("warning-lbl").innerHTML = numberOfFolders;
+  }
 
 }
 
+
+
+
 document.getElementById("pick-directory-btn").addEventListener("click", pickWorkingDirectory);
+document.getElementById("create-directory-btn").addEventListener("click", createImageFolder);
